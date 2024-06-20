356cbaeeb38b3062857585914ef353d13ba7a069
Gabriel Caetano <gabrielc0202@hotmail.com>
2024-06-20 20:12:12 -0300

    update

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -24,6 +24,22 @@ class TaskController{
             Res.json({error: "invalid status parameter"});
         }
     }
+
+    getById(Req: Request, Res: Response) {
+        const { id_task } = Req.params;
+
+        if (id_task) {
+
+            const result = taskService.getById(id_task);
+
+            Res.json(result);
+            Res.status(200);
+
+        }else{
+            Res.json({error:"invalid id_task param"});
+            Res.status(401);
+        }
+    }
     add(Req: Request, Res: Response){
         const {id, descricao, data, status} = Req.body;
 
@@ -42,6 +58,34 @@ class TaskController{
             Res.status(401);
         }
 
+    }
+
+    update(Req: Request, Res: Response) {
+        const { id, descricao, data, status } = Req.body;
+        const { id_task } = Req.params;
+
+        if (id && descricao && data && status && id_task) {
+
+            if(status === "in_progress" || status === "completed") {
+
+                const result = taskService.update(Req.body, id_task);
+
+                if(Object.keys(result).length > 0){
+                    Res.json(result);
+                }else{
+                    Res.json({error: "Task not found"});
+                    Res.status(404);
+                }
+
+            }else{
+                Res.json({error: "Invalid status parameter"});
+                Res.status(401);
+            }
+
+        }else{
+            Res.json({error: "Invalid parameters"});
+            Res.status(401);
+        }
     }
 }
 
--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -22,8 +22,50 @@ class TaskService {
        return tasks;
     }
 
+    getById(id_task: string): Task | {} {
+        const result = taskRepository.get()
+
+        let task = {};
+
+        result.map((obj) => {
+          if(obj.id === id_task){
+            task = obj;
+          }
+        })
+
+        return task;
+    }
+
+    getIndexById(id_task: string): number {
+       const result = taskRepository.get();
+
+       let position: number = 99999;
+
+       result.map((obj,index) => {
+        if(obj.id === id_task) {
+          position = index;
+        }
+
+       })
+
+       return position;
+    }
+
     add(data: Task): Task{
         return taskRepository.add(data);
+    }
+
+    update(data: Task, id_task: string) {
+        
+        const position = this.getIndexById(id_task);
+
+        if(position !== 99999) {
+            return taskRepository.update(data, position);
+        }else{
+          return {};
+        }
+
+
     }
 }
 

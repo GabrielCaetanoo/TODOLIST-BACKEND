5fc677fe0b43b2a6dfd9663826aaa2f9221c1abe
Gabriel Caetano <gabrielc0202@hotmail.com>
2024-07-08 21:45:32 -0300

    BCrypt

--- index.ts
+++ index.ts
@@ -1,5 +1,24 @@
 import express from 'express';
 import router from './routes';
+import * as bcrypt from 'bcrypt';
+
+const pass = "minhasenha123";
+const saltRounds = 10;
+
+
+bcrypt.hash(pass, saltRounds, (error, hash) => {
+
+    //Salvaria a senha no banco de dados
+
+    bcrypt.compare(pass, hash, (error, result) => {
+        if(result) {
+            console.log('Senha correta');
+        }else{
+            console.log('Senha incorreta');
+        }
+    })
+});
+
 
 const server = express();
 

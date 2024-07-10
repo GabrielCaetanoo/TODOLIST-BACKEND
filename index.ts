f7ff72088197eb983113a1accfa85bcecf25b322
Gabriel Caetano <gabrielc0202@hotmail.com>
2024-07-09 21:49:08 -0300

    JWT (JSON Web Token)

--- index.ts
+++ index.ts
@@ -1,6 +1,23 @@
 import express from 'express';
 import router from './routes';
 import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+
+const payload = {
+    userId: 'a36b7a2f-7a7e-4b7e-92d1-97c028058f5e',
+    name: 'Gabriel Caetano'
+}
+
+const secret = 'ef302e1b-7a60-41db-9c08-4d41ff714db8';
+
+const option = {
+    expiresIn: '1h'
+}
+
+const token = jwt.sign(payload, secret, option);
+
+console.log(token);
+
 
 const pass = "minhasenha123";
 const saltRounds = 10;
